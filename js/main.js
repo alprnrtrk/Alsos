0b3ab104c89d58c5ed69372be7bb05b45b0775bc
alprnrtrk <alperenertrk@yahoo.com>
2023-07-28 11:36:33 +0300

    .

--- js/main.js
+++ js/main.js
@@ -1,28 +1,48 @@
 // when the document ready start this function
 
-$(document).ready(function(){
+$(document).ready(function () {
     // event listener for "data-role" elements
-    
-    $("[data-role]").on("click", function(){
+
+    $("[data-role]").on("click", function () {
         // get the role attribute of a clicked element ESO <3
         var role = $(this).attr("data-role");
 
         // checked the role 
-        switch(role){
+        switch (role) {
             // if role is toggler
             case "toggler":
                 // go to toggler function with tihs as object
                 toggler(this);
                 break;
+            case "playPause":
+                playPause(this);
+                break;
+
         }
     })
 })
 
-function toggler(object){
+function toggler(object) {
     // get clicked elemetns target attribute
     let target = $(object).attr("data-target");
 
-    
+
     $(object).toggleClass("active");
     $(target).toggleClass("active");
+}
+
+// play and stop videos
+function playPause(object) {
+    // get clicked elemetns target attribute
+    let target = $(object).attr("data-target");
+
+    if ($(target).get(0).paused) {
+        $(object).find("img").attr("src", "img/icons/pause.svg");
+        
+        $(target).trigger('play');
+    } else {
+        $(object).find("img").attr("src", "img/icons/play.svg");
+
+        $(target).trigger('pause');
+    }
 }
